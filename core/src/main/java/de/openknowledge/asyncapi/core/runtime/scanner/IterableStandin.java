ab33f839b5876f4b1926fff3f5fb15822b317dfd
max.weis <max.weis@openknowledge.de>
2021-01-14 12:08:51 +0100

    remove unused classes

--- core/src/main/java/de/openknowledge/asyncapi/core/runtime/scanner/IterableStandin.java
+++
@@ -1,5 +0,0 @@
-package de.openknowledge.asyncapi.core.runtime.scanner;
-
-public abstract class IterableStandin<E> implements Iterable<E> {
-    E value;
-}
--- core/src/main/java/de/openknowledge/asyncapi/core/runtime/scanner/MapStandin.java
+++
@@ -1,9 +0,0 @@
-package de.openknowledge.asyncapi.core.runtime.scanner;
-
-import java.util.Map;
-
-public abstract class MapStandin<K, V> implements Map<K, V> {
-    K key;
-
-    V value;
-}
