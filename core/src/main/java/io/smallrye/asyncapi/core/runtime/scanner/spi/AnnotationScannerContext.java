10238e78a8872188573f3a9e6e54eb4babfaa1e3
max.weis <max.weis@openknowledge.de>
2021-01-27 09:11:05 +0100

    read schemas from AnnotationScannerContext

--- core/src/main/java/io/smallrye/asyncapi/core/runtime/scanner/spi/AnnotationScannerContext.java
+++ core/src/main/java/io/smallrye/asyncapi/core/runtime/scanner/spi/AnnotationScannerContext.java
@@ -1,29 +1,35 @@
 /*
- * Copyright (C) open knowledge GmbH
+ * Copyright 2019 Red Hat
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an "AS IS" BASIS,
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions
- * and limitations under the License.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+
 package io.smallrye.asyncapi.core.runtime.scanner.spi;
 
+import java.util.ArrayDeque;
 import java.util.Collections;
+import java.util.Deque;
 import java.util.List;
 
+import io.smallrye.asyncapi.core.api.models.AsyncAPIImpl;
+import io.smallrye.asyncapi.spec.models.AsyncAPI;
 import org.jboss.jandex.IndexView;
 
 import io.smallrye.asyncapi.core.api.AsyncApiConfig;
 import io.smallrye.asyncapi.core.runtime.scanner.AnnotationScannerExtension;
 import io.smallrye.asyncapi.core.runtime.scanner.FilteredIndexView;
 import io.smallrye.asyncapi.core.runtime.scanner.dataobject.AugmentedIndexView;
+import org.jboss.jandex.Type;
 
 /**
  * Context for scanners.
@@ -39,19 +45,24 @@ public class AnnotationScannerContext {
     private final AsyncApiConfig config;
 
     private final ClassLoader classLoader;
+    private final AsyncAPI asyncAPI;
+    private final Deque<Type> scanStack = new ArrayDeque<>();
 
     public AnnotationScannerContext(FilteredIndexView index, ClassLoader classLoader,
-            List<AnnotationScannerExtension> extensions,
-            AsyncApiConfig config) {
+        List<AnnotationScannerExtension> extensions,
+        AsyncApiConfig config,
+        AsyncAPI asyncAPI) {
         this.index = index;
         this.augmentedIndex = AugmentedIndexView.augment(index);
         this.classLoader = classLoader;
         this.extensions = extensions;
         this.config = config;
+        this.asyncAPI = asyncAPI;
     }
 
-    public AnnotationScannerContext(IndexView index, ClassLoader classLoader, AsyncApiConfig config) {
-        this(new FilteredIndexView(index, config), classLoader, Collections.emptyList(), config);
+    public AnnotationScannerContext(IndexView index, ClassLoader classLoader,
+        AsyncApiConfig config) {
+        this(new FilteredIndexView(index, config), classLoader, Collections.emptyList(), config, new AsyncAPIImpl());
     }
 
     public FilteredIndexView getIndex() {
@@ -74,10 +85,11 @@ public class AnnotationScannerContext {
         return classLoader;
     }
 
-    @Override
-    public String toString() {
-        return "AnnotationScannerContext{" + "index=" + index + ", augmentedIndex=" + augmentedIndex + ", extensions="
-                + extensions
-                + ", config=" + config + ", classLoader=" + classLoader + '}';
+    public AsyncAPI getAsyncAPI() {
+        return asyncAPI;
+    }
+
+    public Deque<Type> getScanStack() {
+        return scanStack;
     }
 }
